a6200e61896bb50d628ffdbd8a1bf32edc526884
John Davies <jd.other@hotmail.com>
2024-02-20 14:17:23 +1300

    added about and 404 route

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send('Home Page')
+  console.log("user visited default route");
+  res.status(200).send("Home Page");
+});
+
+app.get("/about", (req, res) => {
+  console.log("user visited about route");
+  res.status(200).send("About");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Resource not found</h1>");
 });
 
 app.listen(8080, () => {
