a46439fcda601a0aa7fb9bcf7d1b7384b50ba1c5
John Davies <jd.other@hotmail.com>
2024-02-20 15:55:53 +1300

    basic API set up

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
 
-app.use(express.static("./public"));
-
 app.get("/", (req, res) => {
-  console.log("user visited default route");
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-});
-
-app.get("/about", (req, res) => {
-  console.log("user visited about route");
-  res.status(200).send("About");
-});
-
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>Resource not found</h1>");
+  res.json([{name: 'john'}, {name: 'susan'}]);
 });
 
 app.listen(8080, () => {
