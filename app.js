aab02de157765cff973fe38a9b453473ae02fa92
John Davies <jd.other@hotmail.com>
2024-02-20 15:25:29 +1300

    added navbar-app

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
+
+app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
   console.log("user visited default route");
-  res.status(200).send("Home Page");
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
 app.get("/about", (req, res) => {
