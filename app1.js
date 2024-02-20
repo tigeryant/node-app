654c9a3d5b7eede8d588ad1cab5d1a1338ed1121
John Davies <jd.other@hotmail.com>
2024-02-20 20:55:08 +1300

    implemented middleware

---
+++ app1.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const { products } = require("./data");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product does not exist");
+  }
+  return res.json(singleProduct);
+});
+
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    return res.status(200).send("No products matched your search");
+  }
+  res.status(200).json(sortedProducts);
+});
+
+app.listen(8080, () => {
+  console.log("server is listening on port 8080...");
+});
