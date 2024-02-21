3b2a37d3d1a8f410def5afdc4fc2613f4a451ec9
John Davies <jd.other@hotmail.com>
2024-02-21 17:16:43 +1300

    refactored to controller

--- routes/people.js
+++ routes/people.js
@@ -1,51 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-let { people } = require('../data')
+const { 
+  getPeople,
+  createPerson,
+  createPersonPostman,
+  updatePerson,
+  deletePerson
+} = require('../controllers/people')
 
-router.get('/', (req, res) => {
-  res.status(200).json({ success: true, data: people })
-})
-
-router.post('/', (req, res) => {
-  const { name } = req.body
-  if (!name) {
-    return res.status(400).json({ success: false, msg: 'Please provide a name value'})
-  }
-  res.status(201).json({ success: true, person: name })
-})
-
-router.post('/postman', (req, res) => {
-  const { name } = req.body
-  if (!name) {
-    return res.status(400).json({ success: false, msg: 'Please provide a name value'})
-  }
-  res.status(201).send({ success: true, data: [...people, name]})
-})
-
-router.put('/:id', (req, res) => {
-  const { id } = req.params
-  const { name } = req.body
-  const person = people.find((person) => person.id === Number(id))
-  if (!person) {
-    return res.status(400).json({ success: false, msg: 'Please provide a name value'})
-  }
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name
-    }
-    return person
-  })
-  res.status(200).json({ success: true, data: newPeople })
-})
-
-router.delete('/:id', (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id))
-  if (!person) {
-    return res.status(400).json({ success: false, msg: `No person with id ${req.params.id}`})
-  }
-  const newPeople = people.filter((person) => person.id !== Number(req.params.id))
-  return res.status(200).json({ success: true, data: newPeople})
-})
+router.get('/', getPeople)
+router.post('/', createPerson)
+router.post('/postman', createPersonPostman)
+router.put('/:id', updatePerson)
+router.delete('/:id', deletePerson)
 
 module.exports = router
