76c2c4e6a0826c200a3caeecec8c1c98ccf26b5c
John Davies <jd.other@hotmail.com>
2024-02-21 14:06:48 +1300

    added another POST method

--- app.js
+++ app.js
@@ -8,8 +8,27 @@ app.use(express.static('./methods-public'))
 // parse form data
 app.use(express.urlencoded({ extended: false }))
 
+//parse json
+app.use(express.json())
+
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people })
+})
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res.status(400).json({ success: false, msg: 'Please provide a name value'})
+  }
+  res.status(201).json({ success: true, person: name })
+})
+
+app.post('/api/postman/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res.status(400).json({ success: false, msg: 'Please provide a name value'})
+  }
+  res.status(201).send({ success: true, data: [...people, name]})
 })
 
 app.post('/login', (req, res) => {
