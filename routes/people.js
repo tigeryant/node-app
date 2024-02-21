497b7b1d2245fcb5a1771c092fa1d7136139722c
John Davies <jd.other@hotmail.com>
2024-02-21 17:48:45 +1300

    added alternative syntax

--- routes/people.js
+++ routes/people.js
@@ -15,4 +15,11 @@ router.post('/postman', createPersonPostman)
 router.put('/:id', updatePerson)
 router.delete('/:id', deletePerson)
 
+// alternative syntax
+/*
+router.route('/').get(getPeople).post(createPerson)
+router.route('/postman').post(createPersonPostman)
+router.route('/:id').put(updatePerson).delete(deletePerson)
+*/
+
 module.exports = router
